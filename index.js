d27adc0634ab9c41174ffb2fe1afd978f343d780
tongnoiza <sontayabootnamruk@gmail.com>
2021-12-18 12:03:55 +0700

    init

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const app = express()
 const port = process.env.PORT || 443
 
 console.log('port '+port)
-var j = { 
-        name:'sontaya',
-        age:19,
-        name:'tong',
-        age:23,
-        nickname:"tongnoi",
-        university:"king's monkut univerity of technology nort bankok" 
+var j = [
+    {    name:'sontaya',
+age:19,
+name:'tong',
+age:23,
+nickname:"tongnoi",
+university:"king's monkut univerity of technology nort bankok" 
+},{
+    name:'tong',
+    lastname:"bootnamruk",
+    email:"son@email.dkmfkdfm"
 }
+]
+    
+
 
 app.get('/test',(req,res)=>{
     res.status =200;
