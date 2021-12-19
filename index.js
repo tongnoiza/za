9072787f6a33cab6fd2f79f6b8547e937d01785e
tongnoiza <sontayabootnamruk@gmail.com>
2021-12-19 22:47:38 +0700

    ii

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 const express = require('express')
+
 const url = require('url')
 var f = require('fs')
 const app = express()
 const port = process.env.PORT || 443
-var t = f.readFileSync('test.ejs','utf-8')
+var t = f.readFileSync('test.ejs', 'utf-8')
 var j = [
-    {    name:'sontaya',
-age:19,
-name:'tong',
-age:23,
-nickname:"tongnoi",
-university:"king's monkut univerity of technology nort bankok" 
-},{
-    name:'tong',
-    lastname:"bootnamruk",
-    email:"son@email.dkmfkdfm"
-}
+    {
+        name: 'sontaya',
+        age: 19,
+        name1: 'tong',
+        age1: 23,
+        nickname: "tongnoi",
+        university: "king's monkut univerity of technology nort bankok"
+    }, {
+        name: 'tong',
+        lastname: "bootnamruk",
+        email: "son@email.dkmfkdfm"
+    }
 ]
-    
+var y = [
+    {
+        "name": "messi",
+        "team": "barcelona"
+    },
+    {
+        "name": "C.ronaldo",
+        "team": "Manu"
+    },
+    {
+        "name": "Alexander trant Arnold",
+        "team": "Liverpool"
+    },
+    {
+        "name": "C.ฟกด",
+        "team": "กากๆ"
+    }
+]
+   console.log(j[0] || j[1])
 
-
-app.get('/test',(req,res)=>{
-   var {query }= req
-    if(query.id){
-        res.json(j[query.id])
-    }else{
+app.get('/test', (req, res) => {
+    var { query } = req
+    if (query.id) {
+        res.json(y[query.id])
+    } else {
         res.end('no args')
     }
-}).get('/log',(req,res)=>{
-res.send(t)
+}).get('/log', (req, res) => {
+    res.send(t)
 })
 
 
 
 
-app.listen(port,()=>{
-console.log('server running at Heroku port '+port)
+app.listen(port, () => {
+    console.log('server running at Heroku port ' + port)
 })
