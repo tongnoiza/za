5ce151a64647dc8375c655a2788d06118d623a15
sontayatong <98791627+sontayatong@users.noreply.github.com>
2022-02-06 10:01:53 +0700

    core-origin

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express')
-
 const url = require('url')
-var f = require('fs')
+const ejs = require('ejs')
+const res = require('express/lib/response')
+
 const app = express()
 const port = process.env.PORT || 443
-var t = f.readFileSync('test.ejs', 'utf-8')
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:443')
+    res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
+    return next()
+ })
+
+
 var j = 
     {
         fullname: 'sontaya Bootnamruk<br>',
-        age: 24+"\n",
+        age: 24,
         Nickname: 'tong',
-
         university: "King Monkut's Univerity of Technology Nort Bangkok",
         C:"College Industrial Technology",
         Skill: "basic Java c html php javascript ",
         OS: "Linux ,Windows",
         "Basic skill":"word powerpoint",
-
-
     }
+
+    app.get("/",(req,res)=>{
+        res.send(j);
+    })
     
-
-app.get('/', (req, res) => {
-    res.contentType('.html')
-        res.json(j)
-})
-
 app.listen(port, () => {
     console.log('server running at Heroku port ' + port)
 })
