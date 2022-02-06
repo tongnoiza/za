08800b3d2a18d20e85ce878c3a4dd99220c00f18
sontayatong <98791627+sontayatong@users.noreply.github.com>
2022-02-06 10:20:31 +0700

    core

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const app = express()
 const port = process.env.PORT || 443
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:443')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
     return next()
  })
-
 
 var j = 
     {
