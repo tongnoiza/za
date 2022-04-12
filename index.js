4e8fdf20d3a905c89d04eea8f82f4b6b250b8a68
tongnoiza <72687744+tongnoiza@users.noreply.github.com>
2022-04-12 16:00:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = express()
 const port = process.env.PORT || 443
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
+    res.header('Access-Control-Allow-Origin')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
     return next()
  })
 
-var j = 
+var j = [
     {
         fullname: 'sontaya Bootnamruk<br>',
         age: 24,
@@ -22,7 +22,36 @@ var j =
         C:"College Industrial Technology",
         Skill: "basic Java c html php javascript ",
         OS: "Linux ,Windows",
-    }
+    },
+    {
+        fullname: 'sontaya Bootnamruk',
+        age: 24,
+        Nickname: 'tong',
+        university: "King Monkut's Univerity of Technology Nort Bangkok",
+        C:"College Industrial Technology",
+        Skill: "basic Java c html php javascript ",
+        OS: "Linux ,Windows",
+    },
+    {
+        fullname: 'sontaya Bootnamruk',
+        age: 24,
+        Nickname: 'tong',
+        university: "King Monkut's Univerity of Technology Nort Bangkok",
+        C:"College Industrial Technology",
+        Skill: "basic Java c html php javascript ",
+        OS: "Linux ,Windows",
+    },
+    {
+        fullname: 'sontaya Bootnamruk',
+        age: 24,
+        Nickname: 'tong',
+        university: "King Monkut's Univerity of Technology Nort Bangkok",
+        C:"College Industrial Technology",
+        Skill: "basic Java c html php javascript ",
+        OS: "Linux ,Windows",
+    },
+
+]
 
     app.get("/",(req,res)=>{
         res.send(j);
