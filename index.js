d21c3e84483798a72e8d1329d848433070444124
tongnoiza <72687744+tongnoiza@users.noreply.github.com>
2022-04-19 12:55:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 443
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin')
+    res.header('Access-Control-Allow-Origin','http://localhost:3000/')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
     return next()
